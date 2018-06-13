63373e8daa7b726f940a3b11c5a35642ca4c0b30
Hercílio Martins Ortiz <hercilio.ortiz@10-41-40-16.0101.epoa.br.iosda.org>
2018-06-13 15:03:19 -0300

    Estrutura do Login modelada

---
+++ topfood-android/app/src/main/java/grupodobaralho/topfood_android/LoginView.java
@@ -0,0 +1,11 @@
+package grupodobaralho.topfood_android;
+
+public interface LoginView {
+    void showProgress();
+
+    void hideProgress();
+
+    void setUsernameError();
+
+    void setPasswordError();
+}

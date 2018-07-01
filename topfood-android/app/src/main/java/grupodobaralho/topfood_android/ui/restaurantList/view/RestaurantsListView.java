c50335c512f7d74b7bebf9f9846b715c4e3537e8
Israel Deorce <israel.idvj@gmail.com>
2018-07-01 03:54:49 -0300

    mudancas hercilias

--- topfood-android/app/src/main/java/grupodobaralho/topfood_android/ui/restaurantList/view/RestaurantsListView.java
+++ topfood-android/app/src/main/java/grupodobaralho/topfood_android/ui/restaurantList/view/RestaurantsListView.java
@@ -4,64 +4,59 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.SearchView;
 import android.widget.Toast;
 
-import java.util.List;
-
 import grupodobaralho.topfood_android.R;
-import grupodobaralho.topfood_android.data.db.model.Restaurant;
+import grupodobaralho.topfood_android.data.prefs.UserBusiness;
 import grupodobaralho.topfood_android.ui.login.view.LoginActivity;
 import grupodobaralho.topfood_android.ui.restaurantList.presenter.IRestaurantsListPresenter;
 import grupodobaralho.topfood_android.ui.restaurantList.presenter.RestaurantsListPresenter;
 
 public class RestaurantsListView extends AppCompatActivity implements IRestaurantsListView, SearchView.OnQueryTextListener{
 
+    // TODO: remover parte logica da da activity > isso inclui o UserBusiness
+    final UserBusiness userBusiness = UserBusiness.getInstance();
+
     private RestaurantsListAdapter adapter;
     private static IRestaurantsListPresenter presenter;
-    private RecyclerView rvRestaurants;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restaurant_list);
+        setContentView(R.layout.activity_main);
 
         SearchView searchView = (SearchView) findViewById(R.id.activity_catalog_search);
         searchView.setOnQueryTextListener(this);
 
-        //Variaveis necessarias para trabalhar com recyclerview
-        rvRestaurants = (RecyclerView) findViewById(R.id.rv_restaurants);
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        rvRestaurants.setLayoutManager(layoutManager);
-
-        if(presenter == null)
+        if(presenter == null){
             presenter = new RestaurantsListPresenter();
+        }
         presenter.setView(this);
         presenter.listAllRestaurants();
     }
 
     @Override
-    public void showRestaurants(List<Restaurant> restaurants) {
-        rvRestaurants.setVisibility(View.VISIBLE);
+    protected void onStart() {
+        super.onStart();
 
-        adapter = new RestaurantsListAdapter(this, restaurants);
+        RecyclerView rvRestaurants = (RecyclerView) findViewById(R.id.rv_restaurants);
+        rvRestaurants.setHasFixedSize(true);
+
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(2, RecyclerView.VERTICAL);
+        rvRestaurants.setLayoutManager(layoutManager);
+
+        adapter = new RestaurantsListAdapter(this, presenter.getRestaurants());
         rvRestaurants.setAdapter(adapter);
     }
 
     @Override
-    public void showProgressBar() {
-        findViewById(R.id.pb_restaurants).setVisibility(View.VISIBLE);
-    }
-
-    @Override
-    public void hideProgressBar() {
-        findViewById(R.id.pb_restaurants).setVisibility(View.GONE);
+    public void showProgressBar(int visibilidade) {
+        findViewById(R.id.pb_restaurants).setVisibility(visibilidade);
     }
 
     @Override
@@ -76,13 +71,11 @@ public class RestaurantsListView extends AppCompatActivity implements IRestauran
 
     @Override
     public boolean onQueryTextSubmit(String s) {
-        return onQueryTextChange(s);
+        return false;
     }
 
     @Override
     public boolean onQueryTextChange(String s) {
-        adapter.setRestaurants(presenter.searchARestaurant(s));
-        adapter.notifyDataSetChanged();
         return false;
     }
 
@@ -90,7 +83,7 @@ public class RestaurantsListView extends AppCompatActivity implements IRestauran
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_logout, menu);
 
-        if(!presenter.hasUserLogged()) {
+        if(!userBusiness.isLogged()) {
             menu.findItem(R.id.action_logout).setTitle("Fazer Login");
         }
         return super.onCreateOptionsMenu(menu);
@@ -102,10 +95,11 @@ public class RestaurantsListView extends AppCompatActivity implements IRestauran
 
         if (itemId == R.id.action_logout) {
 
-            if(!presenter.hasUserLogged()) {
+            if(!userBusiness.isLogged()) {
                 startActivity(new Intent(this, LoginActivity.class));
             } else {
-                presenter.makeLogout();
+                userBusiness.removeAccessToken();
+                Toast.makeText(this, "Logout realizado com sucesso.", Toast.LENGTH_LONG).show();
                 startActivity(new Intent(this, RestaurantsListView.class));
                 finish();
             }
